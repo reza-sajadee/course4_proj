b88f9116cd0ee70fe453bcb23119beb95c3527a0
Reza Sajadi <reza.sajadee@gmail.com>
2024-03-25 20:15:36 +0000

    Finish celery introduction

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -48,7 +48,8 @@ class Dev(Configuration):
         'django.contrib.messages',
         'django.contrib.staticfiles',
         "movies",
-        "gh"
+        "gh",
+        "django_celery_results"
     ]
 
     MIDDLEWARE = [
@@ -157,3 +158,6 @@ class Dev(Configuration):
             "level": "DEBUG",
         },
     }
+
+    CELERY_RESULT_BACKEND = "django-db"
+    CELERY_BROKER_URL = "redis://localhost:6379/0"
